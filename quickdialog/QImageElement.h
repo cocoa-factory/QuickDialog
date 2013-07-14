fbd406a0669a911e892c4361a3b9ba6432d68bae
Alan K. Duncan <alan@cocoa-factory.com>
2013-07-14 08:05:17 -0500

    documentation

--- quickdialog/QImageElement.h
+++ quickdialog/QImageElement.h
@@ -17,14 +17,41 @@
 #import <UIKit/UIKit.h>
 #import "QRootElement.h"
 
+/** Image element.  
+ 
+ This subclass of QEntryElement declares the programmatic interface to a class of objects that display an
+ image in a form.
+ */
 @interface QImageElement : QEntryElement
 
+/** Returns or sets the UIImage displayed in the cell */
 @property (nonatomic, strong) UIImage *imageValue;
+
+/** Provides access to the imageValue of the cell by name.  Setting this property looks for an image with the provided name 
+ in the main application bundle and sets the imageValue property to that image.  The getter always returns nil.
+ */
 @property (nonatomic, strong) NSString *imageValueNamed;
+
+/** Sets the largest dimension that the element's image can occupy in the cell. */
 @property (nonatomic, assign) float imageMaxLength;
+
+/** Sets or returns the UIImagePickerControllerSourceType used to select a different image for the element if the user
+ selects the cell.
+ */
 @property(nonatomic) UIImagePickerControllerSourceType source;
 
+/** @name Initialization */
 
+/** Designated initializer.
+ 
+ This method returns a new instance of the class with the provided title and detail image.
+ 
+ @param title
+    The title displayed on the cell.
+ @param image
+    The image displayed on the cell.
+ @return The method returns a newly-intialized instance of the class.
+ */
 - (QImageElement *)initWithTitle:(NSString *)title detailImage:(UIImage *)image;
 
 @end
